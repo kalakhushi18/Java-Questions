e857a7669b5f6c7d1df7d3f9e2b15481850757f7
kalakhushi18 <khushikala18@gmail.com>
2021-09-10 05:13:07 +0530

    aster

--- src/com/company/Unionntersection.java
+++ src/com/company/Unionntersection.java
@@ -1,10 +1,101 @@
 package com.company;
 
-public class Unionntersection
-{
-    int l1 , l2 ;
-   int arr[] = new int[] ;
+import java.util.*;
 
+public class Unionntersection {
+    public static void main(String args[]) {
+        int l1, l2;
+        System.out.println("Print values");
+        Scanner sc = new Scanner(System.in);
+        l1 = sc.nextInt();
+        l2 = sc.nextInt();
+        boolean val = false;
+        int a[] = new int[l1];
+        int b[] = new int[l2];
+        ArrayList<Integer> uni = new ArrayList<>();
+        ArrayList<Integer> inter = new ArrayList<>();
+        for (int i = 0; i < l1; i++) {
+            a[i] = sc.nextInt();
+
+        }
+        for (int i = 0; i < l2; i++) {
+            b[i] = sc.nextInt();
+
+        }
+
+        //union
+
+        for (int i = 0; i <= l1; i++) {
+
+
+            if (i < l1) {
+                uni.add(a[i]);
+            } else {
+                if (l1 > l2) {
+                    for (int k = 0; k < l1; k++) {
+                        for (int j = 0; j < l2; j++) {
+                            if (a[k] == b[j]) {
+                                val = true;
+                                inter.add(a[k]);
+                                break;
+                            }
+                        }
+                        if (!val) {
+                            uni.add(a[k]);
+                        } else {
+                            System.out.println(k);
+
+                        }
+
+                    }
+                } else if (l2 > l1) {
+
+
+                    for (int k = 0; k < l2; k++) {
+
+                        for (int j = 0; j < l1; j++) {
+                            if (a[j] == b[k]) {
+                                val = true;
+                                inter.add(b[k]);
+                                break;
+                            } else {
+                                val = false;
+
+                            }
+
+                        }
+                        if (!val) {
+
+                            uni.add(b[k]);
+                        }
+
+                    }
+                } else {
+                    for (int k = 0; k < l1; k++) {
+                        for (int j = 0; j < l2; j++) {
+                            if (a[k] == b[j]) {
+                                val = true;
+                                inter.add(a[k]);
+                                break;
+                            }
+                        }
+                        if (!val) {
+                            uni.add(b[k]);
+                        } else {
+                            System.out.println(k);
+
+                        }
+
+                    }
+                }
+            }
+
+
+        }
+        System.out.println(uni);
+        System.out.println(inter);
+
+    }
 
 
 }
