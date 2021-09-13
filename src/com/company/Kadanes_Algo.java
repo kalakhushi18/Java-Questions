6d516ecf064221930306ddd1f68d034e61b68d28
kalakhushi18 <khushikala18@gmail.com>
2021-09-13 20:32:10 +0530

    Algo

---
+++ src/com/company/Kadanes_Algo.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.*;
+public class Kadanes_Algo {
+
+
+    public static void main(String args[])
+    {
+        int l ,sum=0 ,max =0;
+        Scanner sc = new Scanner(System.in);
+        l = sc.nextInt();
+        int a[] = new int[l];
+        for(int i =0 ;i<l; i++)
+        {
+            a[i]= sc.nextInt();
+        }
+        for(int i =0 ;i<l ;i++)
+        {
+            sum +=a[i];
+            if(sum>max)
+            {
+                max = sum ;
+            }
+            else if(sum<0)
+            {
+                sum =0 ;
+            }
+        }
+        System.out.println(max);
+    }
+
+}
